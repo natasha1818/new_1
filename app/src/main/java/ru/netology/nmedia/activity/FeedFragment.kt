4c9ad5dabcd221a4cbb0de39f3606642bce28177
Natasha K <k_natasha1818@mail.ru>
2023-06-27 19:49:47 +0700

    initial commit

---
+++ app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
@@ -0,0 +1,95 @@
+package ru.netology.nmedia.activity
+
+import android.R.*
+import android.R.color.*
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.OnInteractionListener
+import ru.netology.nmedia.adapter.PostsAdapter
+import ru.netology.nmedia.databinding.FragmentFeedBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+class FeedFragment : Fragment() {
+    private lateinit var runnable: Runnable
+
+    private val viewModel: PostViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentFeedBinding.inflate(inflater, container, false)
+
+            binding.swiperefresh.setOnRefreshListener {
+                runnable = Runnable {
+                    binding.swiperefresh.isRefreshing = false
+                }
+
+                binding.swiperefresh.postDelayed(runnable,6000)}
+
+        binding.swiperefresh.setColorSchemeResources(
+            holo_blue_bright,holo_green_light,holo_orange_light,
+            holo_red_light
+        )
+
+
+        val adapter = PostsAdapter(object : OnInteractionListener {
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+            }
+
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id,post.likedByMe)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, post.content)
+                    type = "text/plain"
+                }
+
+                val shareIntent =
+                    Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                startActivity(shareIntent)
+            }
+        })
+        binding.list.adapter = adapter
+        viewModel.data.observe(viewLifecycleOwner) { state ->
+            adapter.submitList(state.posts)
+            binding.progress.isVisible = state.loading
+            binding.errorGroup.isVisible = state.error
+            binding.emptyText.isVisible = state.empty
+            binding.progress.isVisible = state.refreshing
+        }
+
+        binding.retryButton.setOnClickListener {
+            viewModel.loadPosts()
+
+           // binding.set
+        }
+
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.action_feedFragment_to_newPostFragment)
+        }
+
+        return binding.root
+    }
+
+}
+
+
